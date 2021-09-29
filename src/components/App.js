3520fae1e45f094e75033f6a8c2c806d1690fab0
Elmir Web <elmir.web@gmail.com>
2021-09-29 15:47:01 +0500

    fdsfds

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 
 import LandingOne from "./Landing-One/Landing-One";
+import LandingTwo from "./Landing-Two/Landing-Two";
 
 function App() {
   return (
     <div className="App">
       <LandingOne />
+      <LandingTwo />
     </div>
   );
 }
---
+++ src/components/Landing-Two/Landing-Two.js
@@ -0,0 +1,28 @@
+import "./Landing-Two.scss";
+
+function LandingOne() {
+  return (
+    <div className="Landing-Two">
+      <header>
+        <span className="span-header-name">Крайние работы</span>
+        <p className="p-header-text">Шесть крайних работ из портфолио</p>
+      </header>
+      <main>
+        <div className="cards-portfolio">
+          <div className="card card-1">1</div>
+          <div className="card card-1">2</div>
+          <div className="card card-1">3</div>
+          <div className="card card-1">4</div>
+          <div className="card card-1">5</div>
+          <div className="card card-1">6</div>
+        </div>
+
+        <a href="#" className="a-link-button">
+          Показать все портфолио
+        </a>
+      </main>
+    </div>
+  );
+}
+
+export default LandingOne;
