9d16fccdb25b6928cc70b1ed9add3c242576a751
Elmir Web <elmir.web@gmail.com>
2021-12-22 19:28:38 +0500

    Заменен API на kinopoiskapiunofficial.tech + переписан код всего рендера

--- src/components/my-films/My-Films.jsx
+++ src/components/my-films/My-Films.jsx
@@ -1,116 +1,177 @@
-import React from "react"; // импортируем React
+/* eslint-disable no-loop-func */
+import React, { useEffect, useState } from "react"; // импортируем React
 
 import "./My-Films.scss"; // импортируем стили компонента
+
 import dataMyFilms from "./../../DATAS/MyFilms.json"; // импортируем наш массив с названиями и годами фильмов
 
-// главная функция компонента
 function MyFilms() {
-  const [myFilmsPack, setMyFilmsPack] = React.useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [stateFilms, setFilmsState] = useState([]);
+  let test123 = 0;
 
-  // данный useEffect сработает при монтировании компонента всего лишь один раз
-  React.useEffect(() => {
-    // let testIndex = 0; // TODO: НАВЕРНОЕ ВРЕМЕННО
-    let reqKinopoiskFilm = []; // массив для найденных одинаковых фильмов из нашего массива и запроса
+  let arrayFilms = [];
 
-    // бегаем по нашему массиву с названиями и годами фильмов
-    for (let i = 0; i < dataMyFilms.length; i++) {
-      // запишем во временные переменные название и год фильма на текущей итерации из соответствующего объекта в массиве
-      let tempNameFilm = dataMyFilms[i].name_russian;
-      let tempYearFilm = dataMyFilms[i].year;
+  useEffect(() => {
+    setFilmsState([]);
 
-      // все фильмы кроме последнего в массиве
-      if (i + 1 !== dataMyFilms.length) {
-        // console.log(i + 1); // выводим текущий номер итерации
+    componentStart(setIsLoaded, error, setError, arrayFilms, setFilmsState);
+  }, []); // когда компонент смонтируется, запустим функцию
 
-        // создаем обещание, чтобы ждало
-        new Promise((resolve, reject) => {
-          // сделаем между запросами дистанцию
-          setTimeout(async () => {
-            let queryFetch = await fetch(
-              `https://kinobd.ru/api/films/search/title?q=${tempNameFilm}`
-            ); // отправим запрос на наше апи с названием из нашего массива и запишем ответ
+  if (error) {
+    return (
+      <div className="MyFilms">
+        <h4>Ошибка загрузки любимых фильмов, обновите страницу!</h4>
+        <p>{error}</p>
+      </div>
+    );
+  } else if (!isLoaded) {
+    // загрузка
 
-            // если произошла ошибка
-            if (!queryFetch.ok) {
-              reject(`Ошибка ответа на запрос №${queryFetch.status}`); // зареджектим наш промис
-            } /* если все хорошо и ответ пришел*/ else {
-              let queryJSON = await queryFetch.json(); // запишем ответ в нашу временную переменную
+    return (
+      <div className="MyFilms">
+        <h4>Загружаются мои любимые фильмы...</h4>
+      </div>
+    );
+  } else {
+    // все хорошо
 
-              resolve(queryJSON.data);
-            }
-          }, 1000);
-        })
-          // если все хорошо и промис зарезолвился
-          // eslint-disable-next-line no-loop-func
-          .then((result) => {
-            // console.log("все хорошо");
-
-            // бегаем по полученному массиву для каждого из фильмов
-            result.map(function (itemReqFilm) {
-              let reqNameFilm = itemReqFilm.name_russian; // из полученного массива для каждого из фильмов сохраняем во временную переменную название фильма
-              let reqYearFilm = itemReqFilm.year; // и год тоже
-
-              // условие если название и год фильма из нашего массива схожи с названием и годом фильма из полученного массива
-              if (
-                tempNameFilm === reqNameFilm &&
-                tempYearFilm === reqYearFilm
-              ) {
-                /* то */
-                // testIndex++;
-
-                // console.log(`_______ | ${testIndex} ______`);
-                // console.log(reqNameFilm, reqYearFilm);
-                // console.log("_____________");
-
-                reqKinopoiskFilm.push(itemReqFilm);
-              }
-
-              /*else {
-              }*/
-            });
-
-            setMyFilmsPack(reqKinopoiskFilm);
-          })
-          // отловим нашу ошибку
-          .catch((errResponse) => {
-            // выведем ошибку в консоль если промис зареджектился
-            console.log(errResponse);
-          });
-      } /* последний фильм в массиве */ else {
-        console.log(`все ${i + 1}`);
-      }
-    }
-  }, []);
-
-  myFilmsPack.map(function (item) {
-    console.log(item);
-  });
-
-  return (
-    <div className="MyFilms">
-      <h4>Мои любимые фильмы</h4>
-      {/* <pre>{JSON.stringify(dataMyFilms, null, 2)}</pre> */}
-      <br />
-      {myFilmsPack.map(function (item) {
-        return (
-          <div>
-            <h1>{item.name_russian}</h1>
-            <img src={item.big_poster} alt="" />
-            <p>
-              Страна: {item.country_ru}
-              <br />
-              Описание: {item.description}
-              <br />
-              Слоган: {item.slogan}
-              <br />
-              Год: {item.year}
-            </p>
-            <hr />
-          </div>
-        );
-      })}
-    </div>
-  );
+    return (
+      <div className="MyFilms">
+        <h4>Любимые фильмы:</h4>
+        <br />
+        <div>
+          {stateFilms.map(function (filmState) {
+            test123++;
+            return (
+              <div>
+                <div>
+                  <h1>
+                    {filmState.nameRu} | {filmState.nameEn}
+                  </h1>
+                  <img src={filmState.posterUrl} alt={filmState.nameRu} />
+                  <p>{filmState.description}</p>
+                  <p>
+                    Жанр:{" "}
+                    {filmState.genres.map(function (genre) {
+                      return <span>{genre.genre}, </span>;
+                    })}
+                  </p>
+                  <p>Рейтинг: {filmState.rating}</p>
+                  <p>Год: {filmState.year}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default MyFilms; // экспортируем компонент
+export default MyFilms;
+
+function componentStart(
+  setIsLoaded,
+  error,
+  setError,
+  arrayFilms,
+  setFilmsState
+) {
+  // когда компонент смонтируется, запустим функцию
+
+  for (let i = 0; i < dataMyFilms.length; i++) {
+    // бегаем по нашему массиву с фильмами
+
+    let tempLocalNameFilm = dataMyFilms[i].nameRu;
+    let tempLocalNameENGFilm = null;
+    let tempLocalYearFilm = dataMyFilms[i].year;
+    // получили во временные переменные наше название фильма и год для текущей итерации
+
+    if (dataMyFilms[i].hasOwnProperty("nameEn")) {
+      tempLocalNameENGFilm = dataMyFilms[i].nameEn;
+    }
+
+    if (!error) {
+      let promiseQueryThisFilm = new Promise((resolve, reject) => {
+        // инициализация промиса для асинхронной работы
+        // создаем задержку чтобы не спамить бэк дохрелионом запросов
+        setTimeout(
+          fetchThisFilm, // функция
+          i * 200, // задержка
+          tempLocalNameFilm, // название фильма
+          tempLocalNameENGFilm, // eng name
+          tempLocalYearFilm, // год фильма
+          i, // номер итерации
+          setIsLoaded, // хук Lazzy Loading
+          setError, // хук error
+          arrayFilms, // массив фильмов
+          setFilmsState // хук фильмов
+        );
+      });
+    }
+  }
+}
+
+async function fetchThisFilm(
+  localNameFilm,
+  localNameENGFilm,
+  localYearFilm,
+  localIDFilm,
+  setIsLoaded,
+  setError,
+  arrayFilms,
+  setFilmsState
+) {
+  // функция запустится для каждого фильма из нашего массива
+
+  try {
+    let queryFetch = await fetch(
+      `https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${localNameFilm}`,
+      {
+        method: "GET",
+        headers: {
+          "X-API-KEY": "948aac51-8d03-4f10-8663-c19fe7f7eed9",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!queryFetch.ok) {
+      setError(`Ошибка загрузки фильмов: ${queryFetch.status}`);
+    } else {
+      let responseJSON = await queryFetch.json();
+
+      // eslint-disable-next-line array-callback-return
+      responseJSON.films.map((itemResponse) => {
+        let reqNameFilm = itemResponse.nameRu; // из полученного массива для каждого из фильмов сохраняем во временную переменную название фильма
+        let reqNameENGFilm = itemResponse.nameEn;
+        let reqYearFilm = itemResponse.year; // и год тоже
+
+        if (
+          (localNameFilm === reqNameFilm &&
+            localYearFilm === reqYearFilm &&
+            localNameENGFilm !== null &&
+            localNameENGFilm === reqNameENGFilm) ||
+          (localNameFilm === reqNameFilm &&
+            localYearFilm === reqYearFilm &&
+            localNameENGFilm === null)
+        ) {
+          console.log(itemResponse, localNameFilm, localYearFilm, localIDFilm);
+
+          // setFilmsState((state) => [...state, itemResponse]);
+          arrayFilms.push(itemResponse);
+
+          if (localIDFilm + 1 === dataMyFilms.length) {
+            setIsLoaded(true);
+            setFilmsState(arrayFilms);
+          }
+        }
+      });
+    }
+  } catch (error) {
+    setError(`Ошибка загрузки фильмов: ${error}`);
+  }
+  // finally {}
+}
